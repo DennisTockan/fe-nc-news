2236a50609f090c2841e3f4448dcfdc404265845
Dennis <dennis@Denniss-MacBook-Air.local>
2023-09-11 20:11:24 +0100

    updated filtered pages

--- src/Pages/Topic.jsx
+++ src/Pages/Topic.jsx
@@ -1,25 +1,89 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { getAllArticles } from '../Utils/Api';
+import React from "react";
+import { useState, useEffect } from "react";
+import { getAllArticles } from "../Utils/Api";
 import { useParams } from "react-router-dom";
-import Articles from "./Articles";
+import Card from "../Components/Card";
 
-const  Topic = () => {
-    const { topic } = useParams()
-    const [articles, setArticles] = useState([])
-    const [loading, setLoading] = useState(true);
+const Topic = () => {
+  const { topic } = useParams();
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [sort_by, setSort_by] = useState("votes");
+  const [order, setOrder] = useState("ASC");
 
-    useEffect(() => {
-        const fetchArticles = () => {
-          getAllArticles(topic, sort_by, order).then((articles) => {
-            setArticles(articles);
-            setLoading(false);
-          });
-        };
-        fetchArticles();
-      }, [topic, sort_by, order]);
+  useEffect(() => {
+    const fetchArticles = () => {
+      getAllArticles(topic, sort_by, order).then((articles) => {
+        setArticles(articles);
+        setLoading(false);
+      });
+    };
+    fetchArticles();
+  }, [topic, sort_by, order]);
 
+  return (
+    <>
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <h2 className="lists">
+              {topic[0].toUpperCase() + topic.slice(1)} articles
+            </h2>
 
-}
- 
+            <form className="select">
+              <select
+                value={order}
+                onChange={(e) => setOrder(e.target.value)}
+                className="select-option"
+              >
+                <option value="ASC">Ascending</option>
+                <option value="DESC">Descending</option>
+              </select>
+
+              <select
+                value={sort_by}
+                onChange={(e) => setSort_by(e.target.value)}
+                className="select-option"
+              >
+                <option value="votes">Votes</option>
+                <option value="comment_count">Comment count</option>
+                <option value="created_at">Date</option>
+              </select>
+            </form>
+
+            <ol>
+              {articles.map(
+                ({
+                  article_id,
+                  title,
+                  author,
+                  created_at,
+                  votes,
+                  article_img_url,
+                  comment_count,
+                }) => {
+                  return (
+                    <Card
+                      key={article_id}
+                      title={title}
+                      author={author}
+                      created_at={created_at}
+                      votes={votes}
+                      image={article_img_url}
+                      commentCount={comment_count}
+                      article_id={article_id}
+                    />
+                  );
+                }
+              )}
+            </ol>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
 export default Topic;
